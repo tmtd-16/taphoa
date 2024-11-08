62ccd9afc33855e7bd14807ea7c744d510d0b513
tmtdat <tmtdat16@gmail.com>
2024-11-08 10:51:54 +0700

    capnhat2 8/11

---
+++ click.js
@@ -0,0 +1,22 @@
+// Chi tiết sản phẩm Socola
+document.getElementById("showDetailsSocola").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("productDetailsSocola").style.display = "block";
+  });
+  
+  document.getElementById("hideDetailsSocola").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("productDetailsSocola").style.display = "none";
+  });
+  
+  // Chi tiết sản phẩm Kem ốc quế
+  document.getElementById("showDetailsKem").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("productDetailsKem").style.display = "block";
+  });
+  
+  document.getElementById("hideDetailsKem").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("productDetailsKem").style.display = "none";
+  });
+  
